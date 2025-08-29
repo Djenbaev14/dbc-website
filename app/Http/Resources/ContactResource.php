2f7bfb35de2e74a3514d3202703516ca01db49cb
Djenbaev14 <sjzgtf14@gmail.com>
2025-08-29 17:23:14 +0500

    feagea

--- app/Http/Resources/ContactResource.php
+++ app/Http/Resources/ContactResource.php
@@ -12,10 +12,27 @@ class ContactResource extends JsonResource
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
+    public function toArray($request)
     {
+        // Qaysi tillarni qo‘llab-quvvatlaymiz
+        $supportedLangs = ['uz', 'ru', 'en', 'qr'];
+
+        // Browser / client yuborgan tilni olish
+        $locale = $request->getPreferredLanguage($supportedLangs) ?? 'uz';
+
+        // Har bir info elementini tilga moslab filter qilish
+        $infos = collect($this->infos)->map(function ($info) use ($locale) {
+            return [
+                'icon' => $info['icon'] ?? null,
+                'title' => $info['title'][$locale] ?? $info['title']['uz'] ?? null,
+                'descriptions' => collect($info['descriptions'] ?? [])->map(fn ($desc) => [
+                    'desc' => $desc['desc'],
+                ]),
+            ];
+        });
+
         return [
-            'infos' => $this->infos,
+            'infos' => $infos,
             'location' => $this->location,
         ];
     }
